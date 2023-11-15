374796314021cf58394ffe4aa70d2fd6ec073117
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2023-11-15 19:40:34 +0200

    feat : add login and register components

---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export {Default as Home} from './Home'
+export {Default as Login} from './Login'
+export {Default as Register} from './Register'
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home,Login,Register } from '../pages'
+
+const AppRoutes = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+                
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes
